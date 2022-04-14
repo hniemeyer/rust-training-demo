009193656bed9251ac198c14f73e7c85fed33bac
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2022-04-14 16:51:14 +0200

    Setup cargo workspace with example (#1)
    * create cargo workspace with geoemtry lib
    
    * point for geometry
    
    * add application which uses geometry

---
+++ application/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let point = geometry::point::Point::new(1.0, 2.0);
+    println!("Distance to origin: {}", point.distance_to_origin());
+    println!("x value: {}", point.x);
+}
---
+++ geometry/src/point.rs
@@ -0,0 +1,26 @@
+pub struct Point {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Point {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x, y }
+    }
+
+    pub fn distance_to_origin(&self) -> f32 {
+        (self.x.powf(2.0) + self.y.powf(2.0)).sqrt()
+    }
+}
+
+#[cfg(test)]
+mod point_tests {
+
+    use super::*;
+
+    #[test]
+    fn dist_to_origin() {
+        let point = Point::new(1.0, 0.0);
+        approx::assert_relative_eq!(point.distance_to_origin(), 1.0);
+    }
+}
