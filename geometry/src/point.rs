c8b485ed31873bf5367eaa20e4e828ad749c2b42
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2022-04-19 10:54:53 +0200

    Rectangle shape with example for generic traits (#3)
    * calculate distance to other point
    
    * make point printable
    
    * Rectangle
    
    * tests for rectangle

--- geometry/src/point.rs
+++ geometry/src/point.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub struct Point {
     pub x: f32,
     pub y: f32,
@@ -11,6 +13,16 @@ impl Point {
     pub fn distance_to_origin(&self) -> f32 {
         (self.x.powf(2.0) + self.y.powf(2.0)).sqrt()
     }
+
+    pub fn distance(&self, other: &Point) -> f32 {
+        ((self.x - other.x).powf(2.0) + (self.y - other.y).powf(2.0)).sqrt()
+    }
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}|{})", self.x, self.y)
+    }
 }
 
 #[cfg(test)]
@@ -22,5 +34,11 @@ mod point_tests {
     fn dist_to_origin() {
         let point = Point::new(1.0, 0.0);
         approx::assert_relative_eq!(point.distance_to_origin(), 1.0);
+    }
+
+    #[test]
+    fn dist_to_same_point() {
+        let point = Point::new(1.0, 0.0);
+        approx::assert_relative_eq!(point.distance(&point), 0.0);
     }
 }
---
+++ geometry/src/shapes.rs
@@ -0,0 +1,47 @@
+use crate::point::Point;
+use crate::traits;
+
+pub struct Rectangle {
+    center: Point,
+    length: f32,
+    width: f32,
+}
+
+impl Rectangle {
+    pub fn new(center: Point, length: f32, width: f32) -> Self {
+        Rectangle {
+            center,
+            length,
+            width,
+        }
+    }
+}
+
+impl traits::Shape<f32> for Rectangle {
+    fn area(&self) -> f32 {
+        self.length * self.width
+    }
+
+    fn circumference(&self) -> f32 {
+        2.0 * self.length + 2.0 * self.width
+    }
+}
+
+#[cfg(test)]
+mod rectangle_tests {
+    use crate::traits::Shape;
+
+    use super::*;
+
+    #[test]
+    fn correct_area() {
+        let my_rect = Rectangle::new(Point::new(0.0, 0.0), 2.0, 3.0);
+        approx::assert_relative_eq!(my_rect.area(), 6.0);
+    }
+
+    #[test]
+    fn correct_circumference() {
+        let my_rect = Rectangle::new(Point::new(0.0, 0.0), 2.0, 3.0);
+        approx::assert_relative_eq!(my_rect.circumference(), 10.0);
+    }
+}
