a1b6a7e5af5eff863c1fd3dcc4ff38020746a7b2
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2022-04-22 20:50:39 +0200

    documentation for shape

--- geometry/src/traits.rs
+++ geometry/src/traits.rs
@@ -1,4 +1,10 @@
+/// The shape trait specifies that a shape should
+/// have an area and a circumference. The genereic
+/// type T is the return type of area and circumference
+/// calculation.
 pub trait Shape<T> {
+    /// Calculate the area of the geometric shape.
     fn area(&self) -> T;
+    /// Calculates the circumference of the geometric shape. 
     fn circumference(&self) -> T;
 }
