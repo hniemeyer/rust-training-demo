fca9ed91b3a15ece89ceb3c76cb8b13dfbb99949
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2022-04-23 09:30:26 +0200

    example for pattern matching with numbers

--- patternmatching/src/main.rs
+++ patternmatching/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let my_number = 256;
+    match my_number {
+        x if x < 0 => println!("negative number!"),
+        3 | 5 | 7 | 11 => println!("Small prime number!"),
+        100..=200 => println!("large number"),
+        _ => println!("not a special number!"),
+    }
 }
