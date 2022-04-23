203fbe86a575d35798166c56c91069c65f5efc4f
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2022-04-23 09:38:34 +0200

    expression example

--- patternmatching/src/main.rs
+++ patternmatching/src/main.rs
@@ -5,5 +5,12 @@ fn main() {
         3 | 5 | 7 | 11 => println!("Small prime number!"),
         100..=200 => println!("large number"),
         _ => println!("not a special number!"),
-    }
+    };
+
+    let new_number: i32 = match my_number {
+        0..=1000 => 1,
+        _ => -1,
+    };
+
+    println!("{}", new_number);
 }
