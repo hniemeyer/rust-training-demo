e8b6561b74c3011676d3672f4afeab0e1d242339
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2022-04-24 09:43:23 +0200

    even numbers iterator

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,3 +1,28 @@
+struct EvenNumbers {
+    value: i32,
+}
+
+impl EvenNumbers {
+    fn new() -> Self {
+        Self { value: 2 }
+    }
+}
+
+impl Iterator for EvenNumbers {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let old_value = Some(self.value);
+        self.value += 2;
+        old_value
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result: Vec<i32> = EvenNumbers::new()
+        .skip(10)
+        .map(|x| 3 * x)
+        .take(10)
+        .collect();
+    println!("{:#?}", result);
 }
