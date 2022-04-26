311139ee0011747b52a6da0f708af725dbaea23f
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2022-04-26 07:50:04 +0200

    channels

--- multithreading/src/main.rs
+++ multithreading/src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
@@ -20,5 +21,47 @@ fn main() {
     for child_thread in my_threads {
         // Wait for the thread to finish. Returns a result.
         let _ = child_thread.join();
+    }
+
+    // mpsc is an acronym for multiple producer, single consumer.
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        let mut rng = rand::thread_rng();
+
+        for val in vals {
+            let rand_millis = rng.gen_range(1..1000);
+            thread::sleep(Duration::from_millis(rand_millis));
+            tx1.send(val).unwrap();
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        let mut rng = rand::thread_rng();
+
+        for val in vals {
+            let rand_millis = rng.gen_range(1..100);
+            thread::sleep(Duration::from_millis(rand_millis));
+            tx.send(val).unwrap();
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
     }
 }
