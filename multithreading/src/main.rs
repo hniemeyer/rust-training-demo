a92df38fbbb9993003fa628a2a0a8724eeae76eb
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2022-04-26 07:41:23 +0200

    os threads

--- multithreading/src/main.rs
+++ multithreading/src/main.rs
@@ -1,3 +1,24 @@
+use std::thread;
+use std::time::Duration;
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut my_threads = vec![];
+
+    for i in 1..10 {
+        my_threads.push(thread::spawn(move || {
+            let mut rng = rand::thread_rng();
+            let rand_millis = rng.gen_range(1..100);
+            println!("hi from thread number {}!", i);
+            thread::sleep(Duration::from_millis(rand_millis));
+            println!("Thread number {} slept for {} millis!", i, rand_millis);
+        }));
+    }
+
+    println!("Now we wait for the threads.");
+    for child_thread in my_threads {
+        // Wait for the thread to finish. Returns a result.
+        let _ = child_thread.join();
+    }
 }
