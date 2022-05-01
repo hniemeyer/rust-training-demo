3394ab7a19604a7cf0fe49db51f1896723682cf0
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2022-05-01 20:47:53 +0200

    cargo fmt

--- hdfarray/src/main.rs
+++ hdfarray/src/main.rs
@@ -2,11 +2,11 @@ use hdf5::{File, Result};
 use ndarray::{arr2, s, Array2};
 
 fn write_hdf5() -> Result<()> {
-    let data : Array2::<i32> = arr2(&[
+    let data: Array2<i32> = arr2(&[
         // write a 2-D array of data
-        [1,2],
-        [3,4],
-        [5,6],
+        [1, 2],
+        [3, 4],
+        [5, 6],
     ]);
     let file = File::create("pixels.h5")?; // open for writing
     let group = file.create_group("dir")?; // create a group
@@ -21,7 +21,7 @@ fn write_hdf5() -> Result<()> {
 fn read_hdf5() -> Result<()> {
     let file = File::open("pixels.h5")?; // open for reading
     let ds = file.dataset("dir/pixels")?; // open the dataset
-    let res: Array2::<i32> = ds.read_slice(s![1.., ..])?;
+    let res: Array2<i32> = ds.read_slice(s![1.., ..])?;
     println!("{}", res);
     Ok(())
 }
