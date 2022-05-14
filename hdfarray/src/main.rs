2d8942e61fd5e26bdbd935013017e9142a0f5386
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2022-05-14 10:27:32 +0200

    write more to hdf

--- hdfarray/src/main.rs
+++ hdfarray/src/main.rs
@@ -1,23 +1,29 @@
 use hdf5::{File, Result};
-use ndarray::{arr2, s, Array2, Array};
-use ndarray_rand::RandomExt;
+use ndarray::{arr2, s, Array, Array1, Array2};
 use ndarray_rand::rand_distr::Uniform;
+use ndarray_rand::RandomExt;
 
 fn write_hdf5() -> Result<()> {
-    let data: Array2<i32> = Array::random((25, 5), Uniform::new(0, 10));
+    let data: Array2<u32> = Array::random((25, 5), Uniform::new(0, 255));
+    let axis: Array1<f32> = Array::random(25, Uniform::<f32>::new(0.0, 255.0));
     let file = File::create("pixels.h5")?; // open for writing
-    let group = file.create_group("dir")?; // create a group
-    let builder = group.new_dataset_builder();
-    let _ds = builder
+    let group = file.create_group("sensor")?; // create a group
+    let _ds = group
+        .new_dataset_builder()
         .with_data(&data)
         // finalize and write the dataset
-        .create("pixels")?;
+        .create("measurement")?;
+    let _ds2 = group
+        .new_dataset_builder()
+        .with_data(&axis)
+        // finalize and write the dataset
+        .create("axis")?;
     Ok(())
 }
 
 fn read_hdf5() -> Result<()> {
     let file = File::open("pixels.h5")?; // open for reading
-    let ds = file.dataset("dir/pixels")?; // open the dataset
+    let ds = file.dataset("sensor/measurement")?; // open the dataset
     let res: Array2<i32> = ds.read_slice(s![1.., ..])?;
     println!("{}", res);
     Ok(())
