6ec0b787daee0f5a8bf327ee26a2da4d6f76ecf0
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2022-05-26 17:14:15 +0200

    Serde (#25)
    * serde json and yaml
    
    * csv example

---
+++ serialization/src/main.rs
@@ -0,0 +1,52 @@
+use serde::{Deserialize, Serialize};
+use std::io;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct InterestingObject {
+    position_x: f32,
+    position_y: f32,
+    name: String,
+    damage: Option<u32>,
+    checked: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ObjectList {
+    contents: Vec<InterestingObject>,
+    project_id: u32,
+    creator: String,
+}
+
+fn main() {
+    let data = ObjectList {
+        contents: vec![
+            InterestingObject {
+                position_x: 12.0,
+                position_y: 33.0,
+                name: "Hallo".to_owned(),
+                damage: None,
+                checked: true,
+            },
+            InterestingObject {
+                position_x: 22.0,
+                position_y: 98.0,
+                name: "Welt".to_owned(),
+                damage: Some(45),
+                checked: false,
+            },
+        ],
+        project_id: 12,
+        creator: "Hendrik".to_owned(),
+    };
+
+    let serialized = serde_json::to_string(&data).unwrap();
+    println!("serialized = {}", serialized);
+    let serialized_yaml = serde_yaml::to_string(&data).unwrap();
+    println!("serialized = {}", serialized_yaml);
+
+    let mut wtr = csv::Writer::from_writer(io::stdout());
+    for item in &data.contents {
+        wtr.serialize(item).unwrap();
+    }
+    wtr.flush().unwrap();
+}
