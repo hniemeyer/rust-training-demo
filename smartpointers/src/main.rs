0ff3c5c94e589c1f7397acad789d7d029cdd8749
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2022-06-06 17:58:25 +0200

    smart pointers

---
+++ smartpointers/src/main.rs
@@ -0,0 +1,59 @@
+use std::rc::Rc;
+
+// struct MyDefectList {
+//     value: i32,
+//     next_item: Option<MyDefectList>,
+// }
+
+struct MyList {
+    value: i32,
+    next_item: Option<Box<MyList>>,
+}
+
+//"Destructors" in Rust
+struct MyData {
+    value: i32,
+}
+
+impl Drop for MyData {
+    fn drop(&mut self) {
+        println!("MyData with value =  {} is destroyed", self.value);
+    }
+}
+
+fn main() {
+    // in Rust there are similar smart pointers as in C++
+    // Box<T> for allocating values on the heap
+    //Rc<T>, a reference counting type that enables multiple ownership
+
+    let mut point_to_val = Box::new(32);
+    println!("{}", point_to_val);
+    *point_to_val = 12;
+    println!("{}", point_to_val);
+
+    //MyList needs a smart pointer inside the option because its size is not known at compile time
+    let my_list = MyList {
+        value: 32,
+        next_item: Some(Box::new(MyList {
+            value: 45,
+            next_item: None,
+        })),
+    };
+
+    let mut my_vec = vec![
+        MyData { value: 1 },
+        MyData { value: 2 },
+        MyData { value: 3 },
+    ];
+
+    my_vec.remove(1);
+    my_vec.push(MyData { value: 4 });
+
+    let rc_my_data = Rc::new(MyData { value: 26 });
+    println! {"ref count: {}", Rc::strong_count(&rc_my_data)};
+    {
+        let x = Rc::clone(&rc_my_data);
+        let y = Rc::clone(&rc_my_data);
+        println! {"ref count: {}", Rc::strong_count(&rc_my_data)};
+    }
+}
